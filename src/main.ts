32491215828692ea147a9d4aa0edb0ffda4269e0
NikhilKumar615 <nikhilkumard03@gmail.com>
2026-03-26 14:07:18 +0530

    updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -30,6 +30,7 @@ async function bootstrap() {
   const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('docs', app, swaggerDocument);
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 3000;
+await app.listen(port);
 }
 bootstrap();
